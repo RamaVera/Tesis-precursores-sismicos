b8b985a943dc121f4c393cbf7b1b9d69c589d10f
Ramiro Vera <rverag@fi.uba.ar>
2023-05-01 22:45:03 -0300

    Add hack to set wifi_ps on ZERO. To verify esp_wifi_set_ps(0);

--- Tesis/main/wifi/wifi.c
+++ Tesis/main/wifi/wifi.c
@@ -106,7 +106,7 @@ esp_err_t WIFI_init(void){
         ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
         ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config) );
         ESP_ERROR_CHECK(esp_wifi_set_ps(WIFI_PS_NONE));
-
+        esp_wifi_set_ps(0);
     return ESP_OK;
 }
 
