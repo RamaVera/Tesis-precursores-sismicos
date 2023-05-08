2ecbbfa2cb2aafb02c3c8e2421380a51f17bddb5
Ramiro Vera <rverag@fi.uba.ar>
2023-05-07 22:24:36 -0300

    Add adc functions

--- Tesis/main/adc/adc.c
+++ Tesis/main/adc/adc.c
@@ -5,6 +5,11 @@
 esp_err_t ADC_Init() {
     // Configurar el modo de operación del ADC
     adc1_config_width(ADC_WIDTH_BIT_12);
-    adc1_config_channel_atten(ADC1_CHANNEL_4, ADC_ATTEN_DB_0);
+    adc1_config_channel_atten(ADC1_CHANNEL_5, ADC_ATTEN_DB_0);
+    adc_set_clk_div(8);
     return ESP_OK;
+}
+
+int ADC_GetRaw() {
+    return adc1_get_raw(ADC1_CHANNEL_5);
 }
--- Tesis/main/adc/adc.h
+++ Tesis/main/adc/adc.h
@@ -8,7 +8,19 @@
 #include "driver/adc.h"
 #include "esp_err.h"
 
+#include <string.h>
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+#include "esp_log.h"
+
+typedef struct ADC_t{
+    int data;
+}ADC_t ;
+
 esp_err_t ADC_Init();
+int ADC_GetRaw();
 
 
 #endif //ADC_H
