ea8609dcbbaebe9494500ab8136f567a6d778b03
Ramiro Vera <rverag@fi.uba.ar>
2023-06-25 21:23:10 -0300

    Create Commands Files

---
+++ Tesis/main/command/command.c
@@ -0,0 +1,65 @@
+//
+// Created by Ramiro on 6/11/2023.
+//
+
+#include <string.h>
+#include <esp_log.h>
+#include <esp_err.h>
+#include "command.h"
+static const char TAG[] = "COMMAND";
+
+esp_err_t COMMAND_Parse(char *rawCommand, command_t *commandParsed){
+    if (rawCommand == NULL || strlen(rawCommand) == 0 || commandParsed == NULL) {
+        ESP_LOGE(TAG,"Error parsing command rawCommand is null or empty or commandParsed is null");
+        return ESP_FAIL;
+    }
+    char *token = strtok(rawCommand, " ");
+    if (strcmp(token, COMMAND_HEADER_TO_RETRIEVE_DATA) == 0 ) {
+        commandParsed->header = RETRIEVE_DATA;
+        int * fields[] = {
+                &commandParsed->startYear,
+                &commandParsed->startMonth,
+                &commandParsed->startDay,
+                &commandParsed->startHour,
+                &commandParsed->startMinute,
+                &commandParsed->endYear,
+                &commandParsed->endMonth,
+                &commandParsed->endDay,
+                &commandParsed->endHour,
+                &commandParsed->endMinute
+        };
+        const int num_fields = sizeof(fields)/sizeof(fields[0]);
+        
+        int i = 0;
+        int data;
+        char * endPtr;
+        while (token != NULL && i < num_fields) {
+            token = strtok(NULL, "-");
+            data = strtol(token, &endPtr, 10);
+            if (*endPtr != '\0' || token == endPtr) {
+                ESP_LOGE(TAG,"Error parsing command");
+                return ESP_FAIL;
+            }
+            memcpy(fields[i], &data, sizeof(data));
+            i++;
+        }
+        ESP_LOGI(TAG,"Command parsed: %d-%d-%d %d:%d - %d-%d-%d %d:%d",commandParsed->startYear,commandParsed->startMonth,commandParsed->startDay,commandParsed->startHour,commandParsed->startMinute,commandParsed->endYear,commandParsed->endMonth,commandParsed->endDay,commandParsed->endHour,commandParsed->endMinute);
+        return ESP_OK;
+
+    }
+    else {
+        ESP_LOGE(TAG,"Error command header not supported");
+        return ESP_FAIL;
+    }
+}
+
+
+char * COMMAND_GetHeaderType(command_t aCommand) {
+    switch (aCommand.header) {
+        case RETRIEVE_DATA:
+            return "Retrieve Data Command Header";
+        default:
+            return "Command Not Supported";
+
+    }
+}
---
+++ Tesis/main/command/command.h
@@ -0,0 +1,37 @@
+//
+// Created by Ramiro on 6/11/2023.
+//
+
+#ifndef COMMAND_H
+#define COMMAND_H
+
+#include <esp_err.h>
+#include <esp_log.h>
+#include <string.h>
+
+typedef enum commandHeader{
+    RETRIEVE_DATA
+}commandHeader_t;
+
+typedef struct command {
+    commandHeader_t header;
+    int startYear;
+    int startMonth;
+    int startDay;
+    int startHour;
+    int startMinute;
+    int endYear;
+    int endMonth;
+    int endDay;
+    int endHour;
+    int endMinute;
+} command_t;
+
+static char *const COMMAND_HEADER_TO_RETRIEVE_DATA = "R";
+
+esp_err_t COMMAND_Parse(char *rawCommand,command_t *commandParsed);
+char *COMMAND_GetHeaderType(command_t aCommand);
+
+
+
+#endif //COMMAND_H
