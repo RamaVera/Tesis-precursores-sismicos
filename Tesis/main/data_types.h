5f40fe32e4953669425c2a9c921008e9a57c0cd1
Ramiro Vera <rverag@fi.uba.ar>
2024-06-09 19:53:11 -0300

    Erase unused types due refactoring

--- Tesis/main/data_types.h
+++ Tesis/main/data_types.h
@@ -9,23 +9,9 @@ typedef struct MPU9250_t {
     uint16_t accelX, accelY, accelZ; /*!< Accelerometer raw data */
 } MPU9250_t;
 
-typedef struct MPU9250_time_t {
-    MPU9250_t sample;
-    int hour;
-    int seconds;
-    int min;
-} MPU9250_time_t;
-
 typedef struct ADC_t{
     uint16_t adcX, adcY, adcZ;
 }ADC_t ;
-
-typedef struct ADC_time_t {
-    ADC_t sample;
-    int hour;
-    int seconds;
-    int min;
-} ADC_time_t;
 
 typedef struct SD_t{
     MPU9250_t mpuData;
